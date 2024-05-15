217001d1208b3682c41e43dcd1af7af610f6a703
Kenneth Lau <kenneth.lau@elekta.com>
2024-05-15 15:07:04 +0200

    Ubuntu 18.04 compatability fixed: use pynput instead of keyboard to prevent permission error; update README: submodule 'ml-agents' relative path and permission for files.

--- example_unity.py
+++ example_unity.py
@@ -1,8 +1,24 @@
 import numpy as np
-import keyboard
+from pynput import keyboard
 from mlagents_envs.base_env import ActionTuple
 from mlagents_envs.environment import UnityEnvironment
 from mlagents_envs.side_channel.engine_configuration_channel import EngineConfigurationChannel
+
+current_keys = set()
+
+pressing_key = None
+def on_press(key):
+    # Add the key to the set of currently pressed keys
+    current_keys.add(key)
+    
+    global pressing_key
+    # Convert the set to a frozenset for easy comparison
+    pressing_key = frozenset(current_keys)
+
+def on_release(key):
+    # Remove the key from the set of currently pressed keys
+    if key in current_keys:
+        current_keys.remove(key)
 
 
 def produce_continuous_action(agent_obs):
@@ -11,23 +27,26 @@ def produce_continuous_action(agent_obs):
 
     continuous_actions = np.zeros(18)
     # You can try moving the box into the little square over the bridge
+    if pressing_key is None:
+        return continuous_actions
+    
     try:
-        if keyboard.is_pressed('w'):
+        if keyboard.KeyCode(char='w') in pressing_key:
             continuous_actions[0] = 1
-        if keyboard.is_pressed('a'):
+        if keyboard.KeyCode(char='a') in pressing_key:
             continuous_actions[1] = -1
-        if keyboard.is_pressed('s'):
+        if keyboard.KeyCode(char='s') in pressing_key:
             continuous_actions[0] = -1
-        if keyboard.is_pressed('d'):
+        if keyboard.KeyCode(char='d') in pressing_key:
             continuous_actions[1] = 1
 
-        if keyboard.is_pressed('1'):
+        if keyboard.KeyCode(char='1') in pressing_key:
             continuous_actions[2] = 1
             continuous_actions[5] = 1
-        if keyboard.is_pressed('2'):
+        if keyboard.KeyCode(char='2') in pressing_key:
             continuous_actions[10] = -0.1
             continuous_actions[14] = -0.1
-        if keyboard.is_pressed('3'):
+        if keyboard.KeyCode(char='3') in pressing_key:
             continuous_actions[10] = 0.2
             continuous_actions[14] = 0.2
             continuous_actions[12] = -0.25
@@ -36,7 +55,8 @@ def produce_continuous_action(agent_obs):
             continuous_actions[17] = -1
 
 
-    except:
+    except Exception as e:
+        print("Error pressing the keyboard:", e)
         pass
 
     return continuous_actions
@@ -50,9 +70,10 @@ def produce_discrete_action(agent_obs):
     reset_agent = 0  # 0 keeps going, 1 resets given agent
 
     try:
-        if keyboard.is_pressed('space'):
+        if pressing_key == frozenset([keyboard.Key.space]):
             reset_agent = 1
-    except:
+    except Exception as e:
+        print("Error pressing space:", e)
         pass
 
     return [agent_module, reset_agent]
@@ -65,26 +86,31 @@ def run_env(example_env):
     observation_spec = example_env.behavior_specs.get(behavior_name).observation_specs[0]
     # print(observation_spec.shape)
 
-    for i in range(10000):
-        (decision_steps, terminal_steps) = example_env.get_steps(behavior_name)
-        # Decision steps -> agent info that needs an action this step
-        # Terminal steps -> agent info whose episode has ended
-        nr_agents = len(decision_steps.agent_id)
-        # print(nr_agents)
-        # print(decision_steps.agent_id)
-        action_tuple = ActionTuple()
-        if nr_agents > 0:
-            observations = decision_steps.obs[0]  # Strange structure, but this is how you get the observations array
-            discrete = np.array([produce_discrete_action(observations[i][:]) for i in range(nr_agents)])
-            cont = np.array([produce_continuous_action(observations[i][:]) for i in range(nr_agents)])
-            action_tuple.add_discrete(discrete)
-            action_tuple.add_continuous(cont)
-        else:
-            action_tuple.add_continuous(np.zeros((0, 18)))
-            action_tuple.add_discrete(np.zeros((0, 2)))
+    with keyboard.Listener(
+            on_press=on_press,
+            on_release=on_release) as listener:
 
-        example_env.set_actions(behavior_name, action_tuple)
-        example_env.step()
+        for i in range(10000):
+            (decision_steps, terminal_steps) = example_env.get_steps(behavior_name)
+            # Decision steps -> agent info that needs an action this step
+            # Terminal steps -> agent info whose episode has ended
+            nr_agents = len(decision_steps.agent_id)
+            # print(nr_agents)
+            # print(decision_steps.agent_id)
+            action_tuple = ActionTuple()
+            if nr_agents > 0:
+                observations = decision_steps.obs[0]  # Strange structure, but this is how you get the observations array
+                discrete = np.array([produce_discrete_action(observations[i][:]) for i in range(nr_agents)])
+                cont = np.array([produce_continuous_action(observations[i][:]) for i in range(nr_agents)])
+                action_tuple.add_discrete(discrete)
+                action_tuple.add_continuous(cont)
+            else:
+                action_tuple.add_continuous(np.zeros((0, 18)))
+                action_tuple.add_discrete(np.zeros((0, 2)))
+
+            example_env.set_actions(behavior_name, action_tuple)
+            example_env.step()
+        listener.join()
 
 # You can change viewports by clicking on the game window and using 1 2 3 4
 if __name__ == '__main__':
@@ -93,7 +119,7 @@ if __name__ == '__main__':
     engine.set_configuration_parameters(quality_level=0)
 
     env = UnityEnvironment(
-        file_name=r"C:\Users\Mart9\Workspace\FM-RL-Planning\builds\win\FM-RL-Unity.exe",
+        file_name="/home/selauken/workplace/fm-rl/FM-RL-Planning/builds/linux/env.x86_64",
         no_graphics=False,  # Can disable graphics if needed
         base_port=10030,  # for starting multiple envs
         side_channels=[engine])
