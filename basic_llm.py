d169ac6b2c39679e2dd89ad2dba1817a8ba50cbe
Mart Kartasev <mart.kartasev@gmail.com>
2024-05-30 16:29:02 +0300

    Fix

--- basic_llm.py
+++ basic_llm.py
@@ -1,6 +1,7 @@
 import torch
 from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
 
+
 model_phi3_instruct ="microsoft/Phi-3-mini-4k-instruct"
 
 torch.random.manual_seed(0)
@@ -13,11 +14,9 @@ model = AutoModelForCausalLM.from_pretrained(
 tokenizer = AutoTokenizer.from_pretrained("microsoft/Phi-3-mini-4k-instruct")
 
 messages = [
-
     {"role": "user", "content": "There is an AI agent with the skills Move To, Pick Object, Place Object, Rotate Object, Dock with Charger!"},
-    {"role": "user", "content": "Produce a sequential plan."},
     {"role": "user", "content": "Give short answers as a python list of key value pairs!"},
-    {"role": "user", "content": "Move an Object X from Position A to position B!"},
+    {"role": "user", "content": "Can you produce a sequential plan to move an Object X from Position A to position B?"},
 ]
 
 pipe = pipeline(
