d701d1bc568856a6783f540bde51a4a18b2c9ab3
Mart Kartasev <mart.kartasev@gmail.com>
2024-07-10 17:11:36 +0300

    Basic llm to unity

--- skills_unity.py
+++ skills_unity.py
@@ -1,15 +1,28 @@
+import csv
 import os
 import platform
-import time
 from enum import Enum
 from threading import Thread
 
 import numpy as np
 
 import ik_server
+from basic_llm import PlanModule
 from mlagents_envs.base_env import ActionTuple
 from mlagents_envs.environment import UnityEnvironment
 from mlagents_envs.side_channel.engine_configuration_channel import EngineConfigurationChannel
+
+# Get the current working directory
+current_path = os.getcwd()
+# Detect the operating system
+system = platform.system()
+
+# Dictionary mapping operating systems to their respective executable paths
+exe_paths = {
+    "Darwin": os.path.join(current_path, "builds/mac/build.app/Contents/MacOS/FM-RL-Unity"),
+    "Windows": os.path.join(current_path, r"builds\win\FM-RL-Unity.exe"),
+    "Linux": os.path.join(current_path, "builds/linux/env.x86_64")
+}
 
 
 class Position(Enum):
@@ -31,105 +44,124 @@ class Module(Enum):
     SkillBasedControl = 2
 
 
-def produce_discrete_action(agent_obs):
-    # All obs relative to agent
-    box_pos = agent_obs[0:3]
-    hand_l_pos = agent_obs[3:6]
-    hand_r_pos = agent_obs[6:9]
-    goal_pos = agent_obs[9:12]
-    ramp_l_bottom_pos = agent_obs[12:15]
-    ramp_l_top_pos = agent_obs[15:18]
-    ramp_r_bottom_pos = agent_obs[18:21]
-    ramp_r_top_pos = agent_obs[21:24]
-    bridge_far_pos = agent_obs[24:27]
-    bridge_near_pos = agent_obs[27:30]
-    move_to_done = agent_obs[30]
-    pick_done = agent_obs[31]
+class SkillBasedEnv:
 
-    agent_module = Module.SkillBasedControl.value
-    pick_target = Position.NoTarget.value
-    move_target = Position.Box.value
+    def __init__(self):
+        self.ikserver = Thread(target=ik_server.serve)
+        self.ikserver.start()
 
-    if np.linalg.norm(box_pos) < 1.5 and move_to_done == 1:
-        move_target = Position.NoTarget.value
-        pick_target = Position.Box.value
+        self.plans = PlanModule()
+        self.plans.query("There is a box thats away from the agent. I want the box to be moved to the goal!")
 
-    if np.linalg.norm(np.array(box_pos) - np.array(hand_l_pos)) < 0.2 and pick_done == 1:
-        pick_target = Position.Box.value
-        move_target = Position.Goal.value
+        try:
+            exe_file = exe_paths[system]
+        except KeyError:
+            raise ValueError(f"Unknown operating system: {system}")
 
-    if np.linalg.norm(goal_pos) < 1.5 and move_to_done == 1:
-        move_target = Position.NoTarget.value
-        pick_target = Position.ForceStop.value
+        print(f"Executable path: {exe_file}")
 
-    camera = 2
-    reset_agent = 0
-    return [agent_module, move_target, pick_target, camera, reset_agent]
+        engine = EngineConfigurationChannel()
+        engine.set_configuration_parameters(time_scale=1, width=800,
+                                            height=600)  # Can speed up simulation between steps with this
+        engine.set_configuration_parameters(quality_level=0)
+
+        print("Trying to connect to Unity Environment!")
+        self.env = UnityEnvironment(
+            file_name=exe_file,
+            no_graphics=False,  # Can disable graphics if needed
+            base_port=10030,  # for starting multiple envs
+            side_channels=[engine])
+        print("Unity Environment connected!")
+        self.env.reset()
+
+        self.actions = []
+        self.current_action = ""
+        self.current_target = Position.NoTarget
+
+        self.move_target = Position.NoTarget
+        self.pick_target = Position.NoTarget
+
+    def run_env(self, ):
+        behavior_name = "Lifter?team=0"
+
+        for i in range(1000000):
+            (decision_steps, terminal_steps) = self.env.get_steps(behavior_name)
+            nr_agents = len(decision_steps.agent_id)
+            action_tuple = ActionTuple()
+            if nr_agents > 0:
+                observations = decision_steps.obs[0]
+                discrete = np.array([self.produce_discrete_action(observations[i][:]) for i in range(nr_agents)])
+                action_tuple.add_discrete(discrete)
+                action_tuple.add_continuous(np.zeros((nr_agents, 24)))
+            else:
+                action_tuple.add_continuous(np.zeros((0, 24)))
+                action_tuple.add_discrete(np.zeros((0, 2)))
+
+            self.env.set_actions(behavior_name, action_tuple)
+            self.env.step()
+
+    def produce_discrete_action(self, agent_obs):
+        # All obs relative to agent
+        box_pos = agent_obs[0:3]
+        hand_l_pos = agent_obs[3:6]
+        hand_r_pos = agent_obs[6:9]
+        goal_pos = agent_obs[9:12]
+        ramp_l_bottom_pos = agent_obs[12:15]
+        ramp_l_top_pos = agent_obs[15:18]
+        ramp_r_bottom_pos = agent_obs[18:21]
+        ramp_r_top_pos = agent_obs[21:24]
+        bridge_far_pos = agent_obs[24:27]
+        bridge_near_pos = agent_obs[27:30]
+        move_to_done = agent_obs[30]
+        pick_done = agent_obs[31]
+
+        self.actions = self.parse_plan()
+
+        current_action_done = move_to_done if self.current_action == "Move To" else (pick_done if self.current_action == "Grasp" else 1)
+
+        if current_action_done:
+            if self.current_action == "Move To":
+                self.move_target = Position.NoTarget
+            (self.current_action, self.current_target) = self.parse_action(self.actions)
+
+        if self.current_action == "Move To":
+            self.move_target = self.current_target
+
+        if self.current_action == "Grasp":
+            self.pick_target = self.current_target
+
+        if self.current_action == "Release":
+            self.pick_target = Position.ForceStop
+
+        agent_module = Module.SkillBasedControl.value
+        camera = 2
+        reset_agent = 0
+        return [agent_module, self.move_target.value, self.pick_target.value, camera, reset_agent]
+
+    def parse_action(self, actions):
+        if len(actions) > 0:
+            (skill, target) = actions.pop(0)
+            if "box" in target:
+                target = Position.Box
+            elif "goal" in target:
+                target = Position.Goal
+            return skill, target
+        return "", Position.ForceStop
+
+    def parse_plan(self):
+        if self.plans.output != "":
+            lines = self.plans.output.splitlines()
+            reader = csv.reader(lines)
+            actions = list(reader)
+
+            if actions[0][0].strip() == "skill" or actions[0][0].strip() == "action":
+                actions.pop(0)
+            print(actions)
+            self.plans.output = ""
+            return actions
+        return self.actions
 
 
-def run_env(example_env):
-    behavior_name = "Lifter?team=0"
-    action_spec = example_env.behavior_specs.get(behavior_name).action_spec
-    # print(action_spec.discrete_branches)
-    observation_spec = example_env.behavior_specs.get(behavior_name).observation_specs[0]
-    # print(observation_spec.shape)
-
-    for i in range(10000):
-        (decision_steps, terminal_steps) = example_env.get_steps(behavior_name)
-        # Decision steps -> agent info that needs an action this step
-        # Terminal steps -> agent info whose episode has ended
-        nr_agents = len(decision_steps.agent_id)
-        # print(nr_agents)
-        # print(decision_steps.agent_id)
-        action_tuple = ActionTuple()
-        if nr_agents > 0:
-            observations = decision_steps.obs[0]  # Strange structure, but this is how you get the observations array
-            discrete = np.array([produce_discrete_action(observations[i][:]) for i in range(nr_agents)])
-            action_tuple.add_discrete(discrete)
-            action_tuple.add_continuous(np.zeros((nr_agents, 24)))
-        else:
-            action_tuple.add_continuous(np.zeros((0, 24)))
-            action_tuple.add_discrete(np.zeros((0, 2)))
-
-        example_env.set_actions(behavior_name, action_tuple)
-        example_env.step()
-
-
-# You can change viewports by clicking on the game window and using 1 2 3 4
 if __name__ == '__main__':
-    engine = EngineConfigurationChannel()
-    engine.set_configuration_parameters(time_scale=1, width=800,
-                                        height=600)  # Can speed up simulation between steps with this
-    engine.set_configuration_parameters(quality_level=0)
-
-    # Get the current working directory
-    current_path = os.getcwd()
-    # Detect the operating system
-    system = platform.system()
-
-    # Dictionary mapping operating systems to their respective executable paths
-    exe_paths = {
-        "Darwin": os.path.join(current_path, "builds/mac/build.app/Contents/MacOS/FM-RL-Unity"),
-        "Windows": os.path.join(current_path, r"builds\win\FM-RL-Unity.exe"),
-        "Linux": os.path.join(current_path, "builds/linux/env.x86_64")
-    }
-
-    # Get the executable path based on the operating system
-    try:
-        exe_file = exe_paths[system]
-    except KeyError:
-        raise ValueError(f"Unknown operating system: {system}")
-
-    print(f"Executable path: {exe_file}")
-
-    ikserver = Thread(target=ik_server.serve).start()
-    print("Trying to connect to Unity Environment!")
-    env = UnityEnvironment(
-        file_name=exe_file,
-        no_graphics=False,  # Can disable graphics if needed
-        base_port=10030,  # for starting multiple envs
-        side_channels=[engine])
-    print("Unity Environment connected!")
-    env.reset()
-    run_env(env)
-    env.reset()
+    env = SkillBasedEnv()
+    env.run_env()
